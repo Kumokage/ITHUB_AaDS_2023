8d3f179e691089c3e20dfc8f8baae555957b3799
CHAINSAWXERI <123538769+CHAINSAWXERI@users.noreply.github.com>
2023-02-20 03:25:07 +0300

    Task01-Vaganov. Sorry for being late :)

---
+++ Tasks/Task1/DynamicArray/NotSortedException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicArray
+{
+    internal class NotSortedException : Exception
+    {
+        public NotSortedException()
+        {
+        }
+
+        public NotSortedException(string message)
+            : base(message)
+        {
+        }
+
+        public NotSortedException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
