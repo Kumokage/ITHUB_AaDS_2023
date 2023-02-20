c744147332f1302712963cf1a5526190b65ff455
CHAINSAWXERI <123538769+CHAINSAWXERI@users.noreply.github.com>
2023-02-20 03:30:28 +0300

    Task01-Vaganov. Sorry for being late :)

--- Tasks/Task1/DynamicArray/DArray.cs
+++ Tasks/Task1/DynamicArray/DArray.cs
@@ -1,63 +1,193 @@
+using System;
+using System.Data;
+
 namespace DynamicArray;
 
 public class DArray : Array
 {
-    public DArray() {
+    private bool _is_sorted = false;
+
+    public DArray() 
+    {
         _container = new int[1];
         _count = 0;
     }
 
-    public DArray(int count) {
-        throw new NotImplementedException();
+    public DArray(int count) 
+    {
+        _container = new int[count];
+        _count = count;
     }
 
-    public DArray(int[] container) {
-        throw new NotImplementedException();
+    public DArray(int[] container) 
+    {
+        _container = container;
+        _count = container.Length;
+
+        int[] sortedArray = new int[container.Length];
+        System.Array.Copy(container, sortedArray, container.Length);
+        DoSort(ref sortedArray);
+        if (sortedArray.SequenceEqual(container))
+        {
+            _is_sorted = true;
+        }
     }
 
     public override int this[int index]
     {
-        get => throw new NotImplementedException();
-        set => throw new NotImplementedException();
+        get
+        {
+            if ((index >= _count) || (index < 0))
+            {
+                throw new IndexOutOfRangeException();
+            }
+            return _container![index];
+        }
+
+        set
+        {
+            if ((index >= _count) || (index < 0))
+            {
+                throw new IndexOutOfRangeException();
+            }
+            _container![index] = value;
+            _is_sorted = false;
+        }
     }
 
     public override int Length
     {
-        get => throw new NotImplementedException();
+        get => _count;
     }
 
-    public override void Insert(int value, int position)
+    private void GrowContainer()
     {
-        throw new NotImplementedException();
+        int[]? newContainer = new int[_count + 1];
+        System.Array.Copy(_container!, newContainer, _count);
+        _container = newContainer;
+        _count++;
+    }
+
+    public override void Insert(int value, int index)
+    {
+        if ((index > _count) || (index < 0))
+        {
+            throw new IndexOutOfRangeException();
+        }
+        
+        if(index == _count)
+        {
+            Insert(value);
+            return;
+        }
+
+        GrowContainer();
+        int len = _count - index - 1;
+        System.Array.Copy(_container!, index, _container!, index + 1, len);
+        _container![index] = value;
+        _is_sorted = false;
     }
 
     public override void Insert(int value)
     {
-        throw new NotImplementedException();
+        GrowContainer();
+        _container![_count - 1] = value;
+        _is_sorted = false;
     }
 
     public override void Remove(int index)
     {
-        throw new NotImplementedException();
+        if ((index >= _count) || (index < 0))
+        {
+            throw new IndexOutOfRangeException();
+        }
+
+        int len = _count - index - 1;
+        System.Array.Copy(_container!, index + 1, _container!, index, len);
+        _count--;
+        _is_sorted = false;
     }
 
     public override void Remove()
     {
-        throw new NotImplementedException();
+        _count--;
+        _is_sorted = false;
     }
 
     public override int BinarySearch(int value)
     {
-        throw new NotImplementedException();
+        if (!_is_sorted)
+        {
+            //throw new NotSortedException();
+            throw new ArgumentException("Not sorted");
+        }
+
+        return BinarySearch(value, 0, _count);
+    }
+
+    private int BinarySearch(int value, int start, int end)
+    {
+        int mid = (start + end) / 2;
+
+        if (start > end)
+        {
+            throw new ArgumentException("No such value");
+        }
+            
+        if (_container![mid] == value)
+        {
+            return mid;
+        }
+
+        if (_container[mid] > value)
+        {
+            return BinarySearch(value, start, mid - 1);
+        }
+        else
+        {
+            return BinarySearch(value, mid + 1, end);
+        }
+            
     }
 
     public override int LinearSearch(int value)
     {
-        throw new NotImplementedException();
+        for (int i = 0; i < _count; i++)
+        {
+            if (value == _container![i])
+            {
+                return i;
+            }
+        }
+
+        throw new ArgumentException("No such value");
+    }
+
+    private void DoSort(ref int[] arr)
+    {
+        int minIndex = 0;
+
+        for (int j = 0; j < _count; j++)
+        {
+            int min = int.MaxValue;
+
+            for (int i = j; i < _count; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                    minIndex = i;
+                }
+            }
+            int s = arr[j];
+            arr[j] = arr[minIndex];
+            arr[minIndex] = s;
+        }
     }
 
     public override void Sort()
     {
-        throw new NotImplementedException();
+        DoSort(ref _container!);
+        _is_sorted = true;
     }
 }
--- Tasks/Task1/DynamicArray/Program.cs
+++ Tasks/Task1/DynamicArray/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace DynamicArray
 {
@@ -7,18 +9,87 @@ namespace DynamicArray
         public delegate int SearchDel(int value);
         public delegate void SortDel();
 
-        public static string[] TestSearchAlgo(SearchDel algo)
+        public static double TestSearchAlgo(SearchDel algo, int N, int searchNumber)
         {
-            throw new NotImplementedException();
+            Stopwatch stopWatch = new Stopwatch();
+            long[] deltas = new long[N];
+            for (int i = 0; i < N; i++)
+            {
+                stopWatch.Start();
+                algo(searchNumber);
+                stopWatch.Stop();
+                deltas[i] = stopWatch.ElapsedTicks;
+                stopWatch.Reset();
+            }
+
+            return deltas.Average();
         }
 
-        public static string[] TestSortAlgo(SortDel algo) {
-            throw new NotImplementedException();
+        public static double TestSortAlgo(SortDel algo, int N) 
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            long[] deltas = new long[N];
+            for (int i = 0; i < N; i++)
+            {
+                stopWatch.Start();
+                algo();
+                stopWatch.Stop();
+                deltas[i] = stopWatch.ElapsedTicks;
+                stopWatch.Reset();
+            }
+
+            return deltas.Average();
         }
 
         static void Main(string[] args)
         {
-            throw new NotImplementedException();
+            int N = 1000; // repeats
+            int min = 0;
+            int max = 10000;
+
+            double avgSort, avgSearchLinear, avgSearchBinary;
+            int[] arr;
+
+
+            string fileName = "AlgoTimeResults.csv";
+            using (StreamWriter sw = File.CreateText(fileName))
+            {
+                sw.WriteLine("n,LinearSearch,BinarySearch,Sort");
+            }
+
+            for (int n = 500; n < 10000; n += 500)
+            {
+                arr = FillWithRandom(min, max, n);
+                DArray testArrSort = new DArray(arr);
+                avgSort = TestSortAlgo(testArrSort.Sort, N);
+
+                arr = FillWithRandom(min, max, n);
+                int searchNumber = arr[n/2]; // the number we are searching we take from middle of array
+                DArray testArrSearch = new DArray(arr);
+                testArrSearch.Sort();
+                avgSearchLinear = TestSearchAlgo(testArrSearch.LinearSearch, N, searchNumber);
+                avgSearchBinary = TestSearchAlgo(testArrSearch.BinarySearch, N, searchNumber);
+
+                using (StreamWriter sw = File.AppendText(fileName))
+                {
+                    sw.WriteLine($"{n},{avgSearchLinear},{avgSearchBinary},{avgSort}");
+                }
+            }
+
+
+        }
+
+        private static int[] FillWithRandom(int min, int max, int n)
+        {
+            int[] arr = new int[n];
+            Random randNum = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = randNum.Next(min, max);
+            }
+
+            return arr;
         }
     }
 }
+
