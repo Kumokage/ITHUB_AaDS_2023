776fedf2eac9dc49ffadaf450a95f790407436e9
lakdjs <qwerty18112005@gmail.com>
2023-03-31 17:26:10 +0300

    Add Task4 implimentation

--- Tasks/Task4/StackTask/Card.cs
+++ Tasks/Task4/StackTask/Card.cs
@@ -1,19 +1,54 @@
 namespace StackTask;
 
-public enum Element
+public enum CardElement
 {
-
+   Water,
+   Fire,
+   Earth,
+   Air,
+   Light,
+   Darkness
 }
 
 public enum CardType
 {
-
+    Creature,
+    Spell,
+    InstantSpell,
+    Territory
 }
 
 public class Card
 {
-    public Card(Element elem, CardType type, string text, int prior)
+    private CardElement _cardElement;
+    private CardType _cardType;
+    private string _text;
+    private int _prior;
+    public Card(CardElement elem, CardType type, string text, int prior)
     {
-
+        cardElement = elem;
+        cardType = type;
+        _text = text;
+        _prior = prior;
+    }
+    public override string ToString() 
+    {
+        return $"\t\n {_cardElement} \n\t {_cardType} \n {_text} \n"
+    }
+    public int Prior
+    {
+        get { return _prior; }
+    }
+    public string Text
+    {
+        get { return _text; }
+    }
+    public CardElement Element
+    {
+        get { return _cardElement; }
+    }
+    public CardType Type
+    {
+        get { return _cardType; }
     }
 }
--- Tasks/Task4/StackTask/CardStack.cs
+++ Tasks/Task4/StackTask/CardStack.cs
@@ -1,34 +1,121 @@
 ﻿namespace StackTask;
+using Card;
 
 public class CardStack
 {
+     class Node
+        {
+            public Card value;
+            public Node? prev;
 
+            
+            public Node(Card value, Node? prev)
+            {
+                this.value = value;
+                this.prev = prev;
+
+            }
+        }
+    private Node? _top;
+    private int _size;
+    
     public int Size
     {
         get
         {
-            throw new NotImplementedException();
+            return _size;
         }
     }
+    public CardStack()
+    {
+            _top = null;
+            _size = 0;
+    }
+
+    public CardStack(IEnumerable<Card> collection)
+        {
+            foreach (Card buf in collection)
+            {
+                Push(buf);
+            }
+        }
 
     public void Push(Card card)
     {
-        throw new NotImplementedException();
+        Node new_card = new(card,null)
+        if(_top is Node top)
+        {
+            Node? buff = top;
+            if(_size>99)
+            {
+                throw new System.Exception("Deck is full");
+            }
+            else
+            {
+                if(card.Prior<buff.value.Prior)
+                {
+                    new_card.prev = _top;
+                    _top = new_card;
+                }
+                else
+                {
+                 while(buff != null && buff.value.Prior <= card.Prior)
+                 {
+                    buff = buff.prev;
+                 } 
+                 if(buff!= null)
+                    {
+                        new_card.prev = buff;
+                    }
+                    else
+                    {
+                        buff.prev = new_card;
+                    }
+                }          
+            }           
+        }
+        else
+        {
+            _top = new_card;
+        }
+        
+        ++_size;
     }
 
     public Card Top()
     {
-        throw new NotImplementedException();
+        if (top is Node node)
+        {
+                return node.value;
+        }
+        else
+        {
+                throw new IndexOutOfRangeException();
+        }
     }
 
     public Card Pop()
     {
-        throw new NotImplementedException();
+            if (top is null)
+            {
+                throw new System.Exception("Empty deck");
+            }
+            Card buf = _top.value;
+            _top = _top.prev;
+            --size;
+            return buf;
     }
 
     public bool IsReadyForGame()
     {
-        throw new NotImplementedException();
+        if(_size>=30)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     public void Shuffle()
