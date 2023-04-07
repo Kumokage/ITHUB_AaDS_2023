d9827c78c9862ffe0756bcc66fe754884d3bccf7
lakdjs <qwerty18112005@gmail.com>
2023-04-07 17:05:33 +0300

    added class card and some of cardcollection

--- Tasks/Task5/MTGCollection/Card.cs
+++ Tasks/Task5/MTGCollection/Card.cs
@@ -1,5 +1,106 @@
 ﻿namespace MTGCollection;
+public enum Color
+{
+    B,
+    G,
+    R,
+    W,
+    U
+}
+public enum Rarity
+{
+    Bonus,
+    Common,
+    Mythic,
+    Rare,
+    Special,
+    Uncommon
+}
+public enum Types
+{
+    Creature,
+    Instant,
+    Sorcery,
+    Enchantment,
+    Land,
+    PlanesWalker,
+    Artifact
+}
 public class Card
 {
+    private int _id;
+    private Color[] _colors = new Color[Enum.GetNames(typeof(Color)).Length];
+    private string _manaCost;
+    private string _name;
+    private int _number;
+    private string _originalText;
+    private int _power;
+    private Rarity _rarity;
+    private string _subtypes;
+    private string _text;
+    private Types[] _types;
 
+    public Card(int id, Color[] colors, string manaCost, string name, int number, string originalText, int power, Rarity rarity, string subtypes, string text, Types[] types)
+    {
+        _id = id;
+        _colors = colors;
+        _manaCost = manaCost;
+        _name = name;
+        _number = number;
+        _originalText = originalText;
+        _power = power;
+        _rarity = rarity;
+        _subtypes = subtypes;
+        _text = text;
+    }
+
+    public int Id
+    {
+        get => _id; 
+    }
+    public Color[] Colors
+    {
+        get => _colors;
+    }
+    public string ManaCost
+    {
+        get => _manaCost;
+    }
+    public string Name
+    {
+        get => _name;
+    }
+    public int Number
+    {
+        get => _number;
+    }
+    public string OriginalText
+    {
+        get => _originalText;
+    }
+    public int Power
+    {
+        get => _power;
+    }
+    public Rarity Rarity
+    {
+        get => _rarity;
+    }
+    public string Subtypes
+    {
+        get => _subtypes;
+    }
+    public string Text
+    {
+        get => _text;
+    }
+    public Types[] Types
+    {
+        get => _types;
+    }
+
+    public override string ToString() 
+    {
+        return $"\t\n {_name}";
+    }
 }
--- Tasks/Task5/MTGCollection/CardCollection.cs
+++ Tasks/Task5/MTGCollection/CardCollection.cs
@@ -1,5 +1,20 @@
+using System.Globalization;
+
 namespace MTGCollection;
 public class CardCollection
 {
-    
+    class Node
+        {
+            public string key;
+            public Card value;
+            public Node? next;
+
+            public Node(Card value, Node? next, string key)
+            {
+                this.value = value;
+                this.next = next;
+                this.key = key;
+            }
+        }
+
 }
