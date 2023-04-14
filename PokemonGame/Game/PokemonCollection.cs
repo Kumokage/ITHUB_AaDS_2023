ee3f5dd8e0152fb1529d8fc9c0e2130f9e0e714f
lakdjs <qwerty18112005@gmail.com>
2023-04-14 17:11:13 +0300

    started collection

--- PokemonGame/Game/PokemonCollection.cs
+++ PokemonGame/Game/PokemonCollection.cs
@@ -3,6 +3,18 @@ namespace PokemonGame;
 
 public class PokemonCollection
 {
+    class Node
+    {
+        public const int COLLECTIONSIZE = 1000;
+        public Pokemon pokemon;
+        public Node? next;
+        public Node(Pokemon pokemon,Node? next,int CONSTSIZE )
+        {
+            this.pokemon = pokemon;
+            this.next = next;
+            COLLECTIONSIZE = CONSTSIZE;
+        }
+    }
     public PokemonCollection(string dataPath)
     {
         throw new NotImplementedException();
