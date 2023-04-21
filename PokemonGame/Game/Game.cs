65f214567c23960ff457ce30bca59eca288de190
Kirillov_Alexei <alexeikirillovigorevich@mail.ru>
2023-04-21 14:21:24 +0300

    Empty commit

--- PokemonGame/Game/Game.cs
+++ PokemonGame/Game/Game.cs
@@ -3,6 +3,7 @@ namespace PokemonGame
     public void Attack(Pokemon attacker, Pokemon defender)
     {
         attacker.AttackPokemon(defender);
-        Console.WriteLine("{0}: HP-{1}", attacker);
+        Console.WriteLine("{0}: HP-{1}", attacker.Name, attacker.Hp);
+        Console.WriteLine("{0}: HP-{1}", defender.Name, defender.Hp);
     }
 }
