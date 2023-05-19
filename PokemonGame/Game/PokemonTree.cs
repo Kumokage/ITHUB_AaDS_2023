f10985d58168a2fcaf9125df23d9e12a5d89ab57
User 316 <User316@infra.ithub.ru>
2023-05-19 18:49:19 +0300

    started delete

--- PokemonGame/Game/PokemonTree.cs
+++ PokemonGame/Game/PokemonTree.cs
@@ -113,38 +113,82 @@ public class PokemonTree
     
     public Pokemon Delete(string name)
     {
-       //if (_root is not null)
-       //{
-       //    Node buff = _root;
-       //    while (buff.right != null && buff.left != null)
-       //    {
-       //        if (buff.value.CompareTo(name) == 1)
-       //        {
-       //            buff = buff.right;
-       //        }
-       //        else if (buff.value.CompareTo(name) == -1)
-       //        {
-       //            buff = buff.left;
-       //        }
-       //        else if (buff.value.CompareTo(name) == 0)
-       //        {
-       //            if (buff.parent != null && buff.parent.right == buff)
-       //            {
-       //                buff.parent.right = buff.left;
-       //                buff.left.parent = buff.parent;
-       //                buff.left.right = buff.right;
-       //            }
-       //            else if(buff.parent != null && buff.parent.left == buff)
-       //            {
-       //                buff.parent.left = buff.left;
-       //                buff.left.parent = buff.parent;
-       //                buff.left.right = buff.right;
-       //            }
-       //            return buff.pokemon;
-       //        }
-       //    }
-       //}
-        throw new NotImplementedException();
+       if (_root is null)
+        {
+            throw new ArgumentException("нет покемонов");
+        }
+        else
+        {
+            Node buff = _root;
+            bool flag = true;
+            while (flag)
+            {
+                if (String.Compare(buff.value, name) > 0)
+                {
+                    if (buff.left != null)
+                    {
+                        buff = buff.left;
+                    }
+                    else
+                    {
+                        flag = false;
+                    }
+                }
+                else if (String.Compare(buff.value, name) < 0)
+                {
+                    if (buff.right != null)
+                    {
+                        buff = buff.right;
+                    }
+                    else
+                    {
+                        flag = false;
+                    }
+                }
+                else if(String.Compare(buff.value, name) == 0)
+                {
+                    if(buff.parent == null)
+                    {
+                        
+                    }
+                    else
+                    {
+                     if(buff.right!= null&& buff.left==null)
+                    {
+                        if(buff.parent.right == buff)
+                        {
+                           buff.parent.right = null;     
+                        }
+                        else
+                        {
+                            buff.parent.left = null;
+                        }
+                    }
+                    if(buff.right== null&& buff.left!=null)
+                    {
+
+                    }
+                    if(buff.right == null&& buff.left == null)
+                    {
+                        if(buff.parent.right == buff)
+                        {
+                           buff.parent.right = null;     
+                        }
+                        else
+                        {
+                            buff.parent.left = null;
+                        }
+                    }
+                    if(buff.right!= null&& buff.left!=null)
+                    {
+
+                    }
+                    return buff.pokemon;
+                    }
+                }
+            }
+            throw new ArgumentException("покемон не нашелся");
+        }
     }
     public void Balance()
     {
