4cd3414ab02d1193bff2958e4f99f91e655aec67
User 316 <User316@infra.ithub.ru>
2023-05-26 18:59:51 +0300

    almost done

--- Tasks/Task3/ListTask/List.cs
+++ Tasks/Task3/ListTask/List.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using System.Collections.Generic;
 using System;
 namespace ListTask 
@@ -7,12 +8,18 @@ namespace ListTask
         class Node
         {
             public int value;
-            public Node next;
-            public Node prev;
+            public Node? next;
+            public Node? prev;
+            public Node(int value,Node?next,Node?prev)
+            {
+                this.value = value;
+                this.next = next;
+                this.prev = prev;
+            }
         }
 
-        private Node head;
-        private Node tail;
+        private Node? head;
+        private Node? tail;
         private int size;
 
         public List() {
@@ -29,25 +36,39 @@ namespace ListTask
             }
         }
 
-        public int Front()
+        public int? Front()
         {
-           return head.value;
+            if(head is not null)
+            {
+            return head.value;
+            }
+            else
+            {
+                return null;
+            }
         }
 
-        public int Back()
+        public int? Back()
         {
+            if(tail is not null)
+            {
             return tail.value;
+            }
+            else
+            {
+                return null;
+            }
         }
 
         public bool Empty()
         {
            if (head == null)
             {
-                return false;
+                return true;
             }
             else
             {
-                return true;
+                return false;
             }
         }
 
@@ -65,9 +86,8 @@ namespace ListTask
 
         public void PushBack(int value)
         {
-            Node newNode = new Node();
-            newNode.value = value;
-            if(head!=null)
+            Node newNode = new Node(value,null,null);
+            if(head!=null&&tail!=null)
             {
                 newNode.prev = tail;
                 tail.next = newNode;
@@ -100,12 +120,15 @@ namespace ListTask
 
                 return temp;
             }
-
+            else
+            {
+                throw new ArgumentException("list is empty");
+            }
+        }
         public void PushFront(int value)
         {
-                Node newNode = new Node();
-                newNode.value = value;
-                if (head != null)
+                Node newNode = new Node(value,null,null);
+                if (head != null&&tail!=null)
                 {
                     newNode.next = head;
                     head.prev = newNode;
@@ -124,9 +147,9 @@ namespace ListTask
             if(head!=null)
                 {
                     int temp = head.value;
-                    if(head.prev != null) 
+                    if(head.next != null) 
                     {
-                        head.prev.next = null;
+                        head.next.prev = null;
                         head = head.next;
                     }
                     else
@@ -135,10 +158,11 @@ namespace ListTask
                         head = null;
                     }
                     --size;
+                    return temp;
                 }
                 else
                 {
-                    throw new Exception("Empty");
+                    throw new Exception("list is empty");
                 }
         }
 
@@ -164,34 +188,93 @@ namespace ListTask
 
         public void Swap(List other_list)
         {
-                List temp = new List(other_list);
-                other_list.Clear();
-                for(int i = 0; i< size; i++)
-                {
-                    other_list.PushBack(i);
-                }
-                Clear();
-                for (int i = 0; i < temp.Size(); i++)
-                {
-                    PushBack(temp[i]);
-                }
-            }
+                List temp = new List();
+                temp.head = other_list.head;
+                temp.tail = other_list.tail;
+                other_list.head = this.head;
+                other_list.tail = this.tail;
+                this.head = temp.head;
+                this.tail = temp.tail;
+        }
 
         public void Remove(int value)
         {
-                if (Empty()==true)
+                if (head == null)
                 {
-                    throw new Exception("Empty");
+                    throw new Exception("Empty list");
                 }
                 else
                 {
+                    Node buff = head;
 
+                    while(buff.next!=null&&buff.value!=value)
+                    {
+                        buff = buff.next;
+                    }
+                        if(buff.value == value)
+                        {
+                            if(buff.prev!=null&&buff.next != null)
+                            {
+                                buff.prev.next = buff.next;
+                                buff.next.prev = buff.prev;
+                                --size;
+                                return;
+                            }
+                            else if(buff.prev != null&&buff.next == null)
+                            {
+                                buff.prev.next = null;
+                                tail = buff.prev;
+                                --size;
+                                return;
+                            }
+                            else if(buff.prev == null&& buff.next == null)
+                            {
+                                head = null;
+                                tail = null;
+                                --size;
+                                return;
+                            }
+                        }
                 }
+                throw new ArgumentException("there's no such element in list");
             }
 
         public void Unique()
         {
-            throw new NotImplementedException();
+            int change = 0;
+            if(head==null || tail==null)
+            {
+                throw new ArgumentException("empty");
+            }
+
+            Node buff = head;
+            while(buff.next != null)
+            {
+                if(buff.next.value == buff.value)
+                {
+                    Node buff2 = buff.next;
+                    while(buff2.next !=null && buff2.next.value == buff.value)
+                    {
+                        buff2 = buff2.next;
+                        ++change;
+                    }
+
+                    if(buff2.next!=null)
+                    {
+                        buff.next = buff2.next;
+                        buff2.next.prev = buff;
+                    }
+                    else
+                    {
+                        buff.next = null;
+                        tail = buff;
+                        return;
+                    }
+
+                }
+                buff = buff.next;
+            }
+
         }
 
         public void Sort()
@@ -203,11 +286,79 @@ namespace ListTask
         {
             get
             {
-                throw new NotImplementedException();
+               int steps = 0;
+               if(index < 0 || index >= size)
+               {
+                   throw new ArgumentException("there's no such index");
+               }
+               else
+               {
+                   if(head!=null&&tail!=null)
+                   {
+                   if(index > size/2)
+                   {
+                       Node buff = tail;
+                       while(buff.prev!=null&&steps != size - index-1)
+                       {
+                           buff = buff.prev;
+                           ++steps;
+                       }
+                       return buff.value;
+                   }
+                   else
+                   {
+                       Node buff = head;
+                       while(buff.next!=null&&steps!=index)
+                       {
+                           buff = buff.next;
+                           ++steps;
+                       }
+                       return buff.value;
+                   }
+               }
+               else
+               {
+                   throw new ArgumentException("list is empty");
+               }
+               }
             }
             set
             {
-                throw new NotImplementedException();
+                int steps = 0;
+               if(index < 0 || index >= size)
+               {
+                   throw new ArgumentException("there's no such index");
+               }
+               else
+               {
+                   if(head!=null&&tail!=null)
+                   {
+                   if(index > size/2)
+                   {
+                       Node buff = tail;
+                       while(buff.prev!=null&&steps != size - index-1)
+                       {
+                           buff = buff.prev;
+                           ++steps;
+                       }
+                        buff.value = value;
+                   }
+                   else
+                   {
+                       Node buff = head;
+                       while(buff.next!=null&&steps!=index)
+                       {
+                           buff = buff.next;
+                           ++steps;
+                       }
+                        buff.value = value;
+                   }
+               }
+               else
+               {
+                   throw new ArgumentException("list is empty");
+               }
+               }
             }
         }
     }
