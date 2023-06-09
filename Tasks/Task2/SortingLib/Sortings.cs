b386b19cea8d05b6e5a5492aa97e565e91eda49e
User 316 <User316@infra.ithub.ru>
2023-06-09 18:14:18 +0300

    finished KT

--- Tasks/Task2/SortingLib/Sortings.cs
+++ Tasks/Task2/SortingLib/Sortings.cs
@@ -13,123 +13,173 @@ namespace SortingLib
 public static void BubleSort(ref string[] arr, Order order)
     {
         int compare = 0;
-        if (order == Order.ASC)
-        {
-            compare = 1;
-        }
-        else if (order == Order.DESC)
-        {
-            compare = -1;
-        }
-
-        for (int i = arr.Length - 1; i > 0; --i)
-        {
-            for (int j = arr.Length - 1; j > 0; --j)
+            if (order == Order.ASC)
             {
-                if (String.Compare(arr[j], arr[j - 1]) == compare)
-                {
-                    string temp = arr[j - 1];
-                    arr[j - 1] = arr[j];
-                    arr[j] = temp;
-                }
+                compare = 1;
             }
-        }
+            else if (order == Order.DESC)
+            {
+                compare = -1;
+            }
+
+                for (int i = 0; i < arr.Length; ++i)
+                {
+                    for (int j = 0; j < arr.Length - i - 1; ++j)
+                    {
+                        if (String.Compare(arr[j + 1], arr[j]) == compare)
+                        {
+                            string buf = arr[j + 1];
+                            arr[j + 1] = arr[j];
+                            arr[j] = buf;
+                        }
+                    }
+                }
     }
 
     public static void SelectSort(ref string[] arr, Order order)
     {
         int compare = 0;
-        if (order == Order.ASC)
-        {
-            compare = 1;
-        }
-        else if (order == Order.DESC)
-        {
-            compare = -1;
-        }
-
-        int max;
-        for (int k = 0; k < arr.Length; k++)
-        {
-            max = 0;
-            for (int i = 1; i < arr.Length - k; i++)
+            if (order == Order.ASC)
             {
-                if (String.Compare(arr[i], arr[max]) == compare)               
-                    max = i;
+                compare = 1;
             }
-            string temp = arr[arr.Length - k - 1];
-            arr[arr.Length - k - 1] = arr[max];
-            arr[max] = temp;
-        }
+            else if (order == Order.DESC)
+            {
+                compare = -1;
+            }
+
+            int minmax;
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                minmax = i;
+                for (int j = i; j < arr.Length; ++j)
+                {
+                    if (String.Compare(arr[j], arr[minmax]) == compare)
+                    {
+                        minmax = j;
+                    }
+                }
+                string buf = arr[i];
+                arr[i] = arr[minmax];
+                arr[minmax] = buf;
+            }
     }
 
     public static void InsertSort(ref string[] arr, Order order)
     {
        int compare = 0;
-        if (order == Order.ASC)
-        {
-            compare = 1;
-        }
-        else if (order == Order.DESC)
-        {
-            compare = -1;
-        }
-
-         string x;
-        int j;
-
-        for (int i = 1; i < arr.Length; i++)
-        {
-            x = arr[i];
-            j = i;
-            while (j > 0 && (string.Compare(arr[j - 1], x)) == compare)
+            if (order == Order.ASC)
             {
-                string temp = arr[j];
-                arr[j] = arr[j - 1];
-                arr[j - 1] = temp;
-                j -= 1;
-
+                compare = 1;
             }
-            arr[j] = x;
-        }
+            else if (order == Order.DESC)
+            {
+                compare = -1;
+            }
+
+            for (int i = 1; i < arr.Length; ++i)
+            {
+                string buff = arr[i];
+                int k = i;
+
+                while (k > 0 && String.Compare(arr[k - 1], buff) != compare)
+                {
+                    arr[k] = arr[k - 1];
+                    --k;
+                }
+                arr[k] = buff;
+            }
     }
-
-
-
-    private static void MergeSort(ref string[] arr, ref string[] buf, int l, int r, int compare)
-    {
-       int compare = 0;
-        if (order == Order.ASC)
+    private static void MergeSort(ref string[] array, ref string[] replic, int l, int r, int compare)
         {
-            compare = 1;
-        }
-        else if (order == Order.DESC)
-        {
-            compare = -1;
-        }
-    }
+            if (l >= r)
+                return;
 
+            int mid = (l + r) / 2;
+            MergeSort(ref array, ref replic, l, mid, compare);
+            MergeSort(ref array, ref replic, mid + 1, r, compare);
+
+            int index = l;
+            for (int i = l, j = mid + 1; i <= mid || j <= r;)
+            {
+                if (j > r || (i <= mid && String.Compare(array[i], array[j]) == compare))
+                {
+                    replic[index] = array[i];
+                    ++i;
+                }
+                else
+                {
+                    replic[index] = array[j];
+                    ++j;
+                }
+                ++index;
+            }
+            for (int i = l; i <= r; ++i)
+            {
+                array[i] = replic[i];
+            }
+        }
     public static void MergeSort(ref string[] arr, Order order)
     {
-        
+       int compare = 0;
+            if (order == Order.ASC)
+            {
+                compare = 1;
+            }
+            else if (order == Order.DESC)
+            {
+                compare = -1;
+            }
+
+            string[] replic = new string[arr.Length];
+
+            MergeSort(ref arr, ref replic, 0, arr.Length - 1, compare);
     }
 
     public static void QuickSort(ref string[] arr,Order order)
     {
        int compare = 0;
-        if (order == Order.ASC)
-        {
-            compare = 1;
-        }
-        else if (order == Order.DESC)
-        {
-            compare = -1;
-        }
-    }
-    private static void QuickSort(ref string[] arr, int start, int end, int compare)
-    {
+            if (order == Order.ASC)
+            {
+                compare = 1;
+            }
+            else if (order == Order.DESC)
+            {
+                compare = -1;
+            }
 
+            QuickSort(ref arr, 0, arr.Length - 1, compare);
     }
+    private static void QuickSort(ref string[] arr, int l, int r, int compare)
+        {
+            if (l < r)
+            {
+                int q = r;
+                string x = arr[l];
+
+                for (int i = l + 1; i <= q;)
+                {
+                    if (String.Compare(x, arr[i]) == compare)
+                    {
+                        string buf = arr[i];
+                        arr[i] = arr[q];
+                        arr[q] = buf;
+
+                        --q;
+                    }
+                    else
+                    {
+                        ++i;
+                    }
+                }
+                string buff = arr[l];
+                arr[l] = arr[q];
+                arr[q] = buff;
+
+                QuickSort(ref arr, l, q - 1, compare);
+                QuickSort(ref arr, q + 1, r, compare);
+            }
+        }
     
     }
    
