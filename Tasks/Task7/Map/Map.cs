209c5fd54abdfc210e83ea843d1027eee97f833f
User 316 <User316@infra.ithub.ru>
2023-06-16 18:49:03 +0300

    graphs

--- Tasks/Task7/Map/Map.cs
+++ Tasks/Task7/Map/Map.cs
@@ -22,12 +22,34 @@ class Map
 
         // You can add constructors here, if you need one.
     }
-
     private Node[] nodes;
 
     public Map(int [,] adjacency_matrix) 
     {
-        throw new NotImplementedException();
+        if(adjacency_matrix.GetLength(0)!=adjacency_matrix.GetLength(1))
+        {
+            throw new ArgumentException("Нужна квадратная матрица");
+        }
+        nodes = new Node[adjacency_matrix.GetLength(0)];
+        for(int i = 0; i < adjacency_matrix.GetLength(0); i++)
+        {
+            nodes[i] = new Node();
+            nodes[i].name = i.ToString();
+        }
+        for(int i = 0; i < adjacency_matrix.GetLength(0); ++i)
+        {
+            for(int j = 0; j < adjacency_matrix.GetLength(1); ++j)
+            {
+                if(adjacency_matrix[i,j]!=0)
+                {
+                    Link newlink = new();
+                    newlink.from = nodes[i];
+                    newlink.to = nodes[j];
+                    newlink.weight = adjacency_matrix[i,j];
+                    nodes[i].linked.Add(newlink);
+                }
+            }
+        }
     }
 
     public Node[] DFS(Node start)
@@ -37,7 +59,30 @@ class Map
 
     public Node[] BFS(Node start) 
     {
-        throw new NotImplementedException();
+        int u = 0;
+        for(int i = 0; i < nodes.Length; i++)
+        {
+            if(nodes[i] == start)
+            {
+                u = i;
+            }
+            else
+            {
+                throw new ArgumentException("Нет такого нода");
+            }
+        }
+       int[][] g = new int[u + 1][];
+       bool[]visited = new bool[u+1];
+       Queue<Node> que = new Queue<Node>();
+       for(int i = 0; i < u + 1; ++i)
+       {
+        g[i] = new int[u+1];
+        for (int j = 0; j < u + 1; j++)
+          {
+                  //g[i][j] = ;
+           }
+       }
+       return null;
     }
 
     public int Path(Node start, Node finish)
